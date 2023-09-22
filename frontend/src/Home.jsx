f69726913facc272b508194584394f3ad6845775
frontend <riyanreza99@gmail.com>
2023-09-22 13:57:12 +0700

    make footer

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -13,6 +13,7 @@ import brand from "./brand_skd.png";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/esm/Button";
+import ListGroup from "react-bootstrap/ListGroup";
 
 import "./App.css";
 
@@ -108,6 +109,57 @@ function Home() {
       </Navbar>
       <Hero />
       <Cardhero />
+      <footer className="bg bg-dark site-footer">
+        <Container>
+          <Row>
+            <Col xs={12} md={6}>
+              <h6>ABOUT</h6>
+              <p className="p-footer">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                excepturi, assumenda repudiandae fugit aut optio. Dolore unde,
+                cum eius voluptas sapiente eveniet vel commodi tempora nostrum
+                quod asperiores hic assumenda.
+              </p>
+            </Col>
+            <Col xs={6} md={3}>
+              <h6>CATEGORIES</h6>
+              <ul className="footer-links">
+                <li>satu</li>
+                <li>dua</li>
+                <li>tiga</li>
+                <li>empat</li>
+              </ul>
+            </Col>
+            <Col xs={6} md={3}>
+              <h6>QUICK LINKS</h6>
+              <ul className="footer-links">
+                <li>coba</li>
+                <li>try</li>
+                <li>try</li>
+                <li>coba</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+        <hr></hr>
+        <Container>
+          <Row>
+            <Col md={8} sm={6} xs={12}>
+              <p className="copyright-text">
+                Copyright &copy; 2017 All Rights Reserved by <a href="#">SKD</a>
+              </p>
+            </Col>
+            <Col md={4} sm={6} xs={12}>
+              <ul className="social-icons">
+                <li>co</li>
+                <li>ba</li>
+                <li>co</li>
+                <li>ba</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </>
   );
 }
